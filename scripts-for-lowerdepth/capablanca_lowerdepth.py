81eaaa5700c6155fee8775340e10e5e2b8dd988c
Dragjon <antoffame@gmail.com>
2023-11-03 20:58:10 +0800

    Adjusted depth

--- scripts-for-lowerdepth/capablanca_lowerdepth.py
+++ scripts-for-lowerdepth/capablanca_lowerdepth.py
@@ -342,7 +342,7 @@ def ai_play_turn(current_board):
   elif(nb_moves > 10 and nb_moves <= 30):
     current_board.push(minimax_root(4, current_board))
   else:
-    current_board.push(minimax_root(6, current_board))
+    current_board.push(minimax_root(5, current_board))
   return
 
 
